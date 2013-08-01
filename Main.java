af10ac8027f4ef29bc3d24352259b2e2a19be808
Devasia Manuel <devasia1000@gmail.com>
2013-08-01 15:41:15 -0400

    code cleanup

--- Main.java
+++ Main.java
@@ -2,31 +2,25 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.*;
 import java.util.*;
-
-
 
 /**
  *
  * @author devasia
  */
-
 public class Main {
 
-    private static String logDirectory="/home/devasia/ourtube/mapping/";
-    
-    private static Hashtable<String, HTTPResponse> table=new Hashtable<String, HTTPResponse>();
-    
-    private static int port=80;
-    
+    private static String logDirectory = "/home/devasia/ourtube/mapping/";
+    private static Hashtable<String, HTTPResponse> table = new Hashtable<>();
+    private static int port = 80;
+
     public static void main(String args[]) throws Exception {
         generateHashTable();
         //printHashTableKeys();
-        
+
         ServerSocket ssock = new ServerSocket(port);
-        System.out.println("Listening on port "+port);
-        int i=0;
+        System.out.println("Listening on port " + port);
+        int i = 0;
         while (true) {
             Socket sock = ssock.accept();
             new Thread(new HTTPPlayback(sock)).start();
@@ -52,44 +46,44 @@ public class Main {
 
             /* find header line */
             String sat[] = new String(d).split("\r\n");
-
+            String headerLine = sat[0];
             /* find the end of the request */
-            int pos=-1;
-            for(int i=0;i<d.length;i++){
-                if(d[i]==13 && d[i+1]==10 && d[i+2]==13 && d[i+3]==10){
-                    pos=i+4;
+            int pos = -1;
+            for (int i = 0; i < d.length; i++) {
+                if (d[i] == 13 && d[i + 1] == 10 && d[i + 2] == 13 && d[i + 3] == 10) {
+                    pos = i + 4;
                     break;
                 }
             }
-            
-            if(pos==-1){
+
+            if (pos == -1) {
                 System.err.println("could not find end of request headers");
                 System.exit(-1);
             }
-            
-            byte[] response=Arrays.copyOfRange(d, pos, d.length);
-            table.put(sat[0], new HTTPResponse(response));
+
+            byte[] response = Arrays.copyOfRange(d, pos, d.length);
+            table.put(headerLine, new HTTPResponse(response));
         }
     }
-    
-    private static void printHashTable(){
-        Enumeration<String> keys=table.keys();
-        Enumeration<HTTPResponse> elements=table.elements();
-        
-        while(keys.hasMoreElements()){
-            System.out.print(keys.nextElement()+"\n\n");
+
+    private static void printHashTable() {
+        Enumeration<String> keys = table.keys();
+        Enumeration<HTTPResponse> elements = table.elements();
+
+        while (keys.hasMoreElements()) {
+            System.out.print(keys.nextElement() + "\n\n");
             System.out.print(elements.nextElement());
         }
     }
-    
-    public static void printHashTableKeys(){
-        Enumeration<String> keys=table.keys();
-        while(keys.hasMoreElements()){
+
+    public static void printHashTableKeys() {
+        Enumeration<String> keys = table.keys();
+        while (keys.hasMoreElements()) {
             System.out.println(keys.nextElement());
         }
     }
-    
-    public static HTTPResponse returnResponse(String requestLine){
+
+    public static HTTPResponse returnResponse(String requestLine) {
         return table.get(requestLine);
     }
 }
