19588624e3e17d62ecd0c9213941123e1a01f5df
Devasia Manuel <devasia1000@gmail.com>
2013-08-01 18:16:43 -0400

    cleaned up code a bit

--- HTTPResponse.java
+++ HTTPResponse.java
@@ -65,7 +65,7 @@ public class HTTPResponse {
         return new String(data);
     }
 
-    public byte[] concat(byte[] first, byte[] second) {
+    public static byte[] concat(byte[] first, byte[] second) {
         byte[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
