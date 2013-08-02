8cb0ee0f06ea453794e42b11e2296a3a7bd0f55d
Devasia Manuel <devasia1000@gmail.com>
2013-08-02 13:37:17 -0400

    removed spurious function

--- HardcodedResponseGenerator.java
+++ HardcodedResponseGenerator.java
@@ -1,29 +1,11 @@
 
-import java.util.Enumeration;
-
-
 /**
  *
  * @author devasia
  */
+import java.util.Enumeration;
+
 public class HardcodedResponseGenerator {
-
-    public static String returnStream204Response() {
-        String resp = "HTTP/1.1 200 OK\r\n"
-                + "Cache-Control:no-cache, must-revalidate\r\n"
-                + "Content-Length:0\r\n"
-                + "Content-Type:text/html; charset=UTF-8\r\n"
-                + "Date:Thu, 01 Aug 2013 14:48:43 GMT\r\n"
-                + "Expires:Fri, 01 Jan 1990 00:00:00 GMT\r\n"
-                + "Pragma:no-cache\r\n"
-                + "Server:Video Stats Server\r\n"
-                + "X-Content-Type-Options:nosniff\r\n"
-                + "X-Frame-Options:SAMEORIGIN\r\n"
-                + "X-XSS-Protection:1; mode=block\r\n"
-                + "\r\n";
-
-        return resp;
-    }
 
     public static String returnVideoplaybackResponse(String mime, int contentLength) {
 
@@ -46,27 +28,27 @@ public class HardcodedResponseGenerator {
 
         return resp;
     }
-    
-    public static HTTPResponse returnHardcodedResponse(String line){
-        HTTPResponse resp=null;
-        String el[]=line.split("\\?");
-        String url=el[0];
-        
-        Enumeration <String> keys=Main.returnHashTableKeys();
-        while(keys.hasMoreElements()){
-            String key=keys.nextElement();
-            String temp[]=key.split("\\?");
-            String u=temp[0];
-            
-            if(u.equals(url)){
-                resp=Main.hashTableLookup(key);
+
+    public static HTTPResponse returnHardcodedResponse(String line) {
+        HTTPResponse resp = null;
+        String el[] = line.split("\\?");
+        String url = el[0];
+
+        Enumeration<String> keys = Main.returnHashTableKeys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            String temp[] = key.split("\\?");
+            String u = temp[0];
+
+            if (u.equals(url)) {
+                resp = Main.hashTableLookup(key);
             }
         }
-        
-        if(resp==null){
+
+        if (resp == null) {
             System.err.println("could not find key in hash table");
         }
-        
+
         return resp;
     }
 }
