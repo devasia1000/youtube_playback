4d55cec143049c5a07496b1c64bb682aa7c8e6c5
Devasia Manuel <devasia1000@gmail.com>
2013-08-02 16:57:35 -0400

    create media directory if it doesnt exist

--- MediaManager.java
+++ MediaManager.java
@@ -62,6 +62,12 @@ public class MediaManager {
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
             if (key.contains("videoplayback")) {
+                /* create media directory if it does not exist */
+                File dir=new File("media");
+                if(!dir.exists()){
+                    dir.mkdir();
+                }
+                
                 //System.out.println(key);
                 String clen = parseClen(key);
                 String type = parseType(key);
@@ -81,7 +87,7 @@ public class MediaManager {
                     if (!f.exists()) {
                         f.createNewFile();
 
-                        System.out.println("downloading and saving to "+f.getAbsolutePath()+", please be patient...");
+                        System.out.println("downloading media stream to "+f.getAbsolutePath()+", please be patient...");
                         Process p = Runtime.getRuntime().exec("wget -O media/" + clen + "-" + type + " " + url);
                         p.waitFor();
                         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
